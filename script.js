1acfc948d5cb601c1fdc85b1c933da5ace13d42b
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-09-06 22:41:06 +0530

    Error solved

--- script.js
+++ script.js
@@ -44,27 +44,13 @@ function levelUp() {
     gameFlash(randBtn);
 }
 
-function reset() {
-    started = false;
-    gameSeq=[];
-    userSeq=[];
-    level = 0;
-}
-
 function checkAns(idx) {
-    // let idx = level-1;
     if (userSeq[idx] === gameSeq[idx]) {
         if  (userSeq.length == gameSeq.length) {
             setTimeout(levelUp, 1000);
         }
     } else {
         h2.innerText = "Game Over! Press any key to start.";
-        
-        // started = false;
-        // gameSeq = [];
-        // userSeq = [];
-        // level = 0;
-        reset();
     }
 }
 
@@ -75,7 +61,7 @@ function  btnPress() {
     userColor = btn.getAttribute("id");
     userSeq.push(userColor);
 
-    checkAns(userSeq.lenght-1);
+    checkAns(userSeq.length-1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
