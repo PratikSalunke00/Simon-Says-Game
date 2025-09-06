9618fcf9db562af3c74c8b5a6cda0cd90f878986
Pratik Salunke <pratiksalunke3112@gmail.com>
2025-09-06 23:26:47 +0530

    Simon Says Game completed

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-let gameSeq=[];
-let userSeq=[];
+let gameSeq = [];
+let userSeq = [];
 
 let btns = ["red", "yellow", "green", "purple"];
 
 let started = false;
 let level = 0;
 
-let h2  = document.querySelector("h2");
+let h2 = document.querySelector("h2");
 
-document.addEventListener("keypress", function() {
+document.addEventListener("keypress", function () {
     if (started == false) {
         console.log("Game is started");
-        started =true;
+        started = true;
 
         levelUp();
     }
@@ -19,16 +19,16 @@ document.addEventListener("keypress", function() {
 
 function gameFlash(btn) {
     btn.classList.add("flash");
-    setTimeout(function() {
+    setTimeout(function () {
         btn.classList.remove("flash");
-    },250);
+    }, 250);
 }
 
 function userFlash(btn) {
     btn.classList.add("userflash");
-    setTimeout(function() {
+    setTimeout(function () {
         btn.classList.remove("userflash");
-    },250);
+    }, 250);
 }
 
 function levelUp() {
@@ -46,30 +46,34 @@ function levelUp() {
 
 function reset() {
     started = false;
-    gameSeq=[];
-    userSeq=[];
+    gameSeq = [];
+    userSeq = [];
     level = 0;
 }
 
 function checkAns(idx) {
     if (userSeq[idx] === gameSeq[idx]) {
-        if  (userSeq.length == gameSeq.length) {
+        if (userSeq.length == gameSeq.length) {
             setTimeout(levelUp, 1000);
         }
     } else {
-        h2.innerText = "Game Over! Press any key to start.";
+        h2.innerHTML = `Game Over! Your score was <b>${level} <br> Press any key to start.`;
+        document.querySelector("body").style.backgroundColor = "red";
+        setTimeout(function () {
+        document.querySelector("body").style.backgroundColor = "white";
+        }, 250); 
         reset();
     }
 }
 
-function  btnPress() {
+function btnPress() {
     console.log(this);
     let btn = this;
     userFlash(btn);
     userColor = btn.getAttribute("id");
     userSeq.push(userColor);
 
-    checkAns(userSeq.length-1);
+    checkAns(userSeq.length - 1);
 }
 
 let allBtns = document.querySelectorAll(".btn");
